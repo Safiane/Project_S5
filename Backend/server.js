33b8215c20f24ab5a799d9a94faaa6b4d931b535
Glatinor <castetspeio@gmail.com>
2025-11-28 09:22:42 +0100

    server.js modifications

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,33 @@
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors()) 
+
+
+const artists = require(path.join(__dirname, '../Frontend/public/data/artists.json'))
+const albums = require(path.join(__dirname, '../Frontend/public/data/albums.json'))
+const songs = require(path.join(__dirname, '../Frontend/public/data/songs.json'))
+
 
 app.get('/api/health', (req, res) => {
   res.json({ status: "ok", message: "Backend skeleton is running" })
 })
 
 app.get('/api/artists', (req, res) => {
-  res.status(501).json({ message: "Not implemented yet" })
+  res.json(artists)
 })
 
 app.get('/api/albums', (req, res) => {
-  res.status(501).json({ message: "Not implemented yet" })
+  res.json(albums)
 })
 
 app.get('/api/songs', (req, res) => {
-  res.status(501).json({ message: "Not implemented yet" })
+  res.json(songs)
 })
 
 app.listen(PORT, () => {
